1accebf7ba6ba08d3b6b5143ca7a98f5d6a262a9
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2024-08-08 15:15:15 -0700

    Improve portability for AppImage

--- common/process.ts
+++ common/process.ts
@@ -1,6 +1,19 @@
+const path = require('path');
+
 export const IS_MAC = process.platform === 'darwin';
 export const IS_WIN = process.platform === 'win32';
 export const IS_DEV = process.env.NODE_ENV === 'development';
 
-export const PORTABLE_DIRECTORY = process.env.PORTABLE_EXECUTABLE_DIR as string;
+let rootDir;
+if (process.env.PORTABLE_EXECUTABLE_DIR) {
+    rootDir = process.env.PORTABLE_EXECUTABLE_DIR;
+} else if (process.env.APPIMAGE) {
+    rootDir = path.dirname(
+        path.resolve(
+            process.env.APPIMAGE
+        )
+    );
+}
+
+export const PORTABLE_DIRECTORY = rootDir;
 export const IS_PORTABLE = !!PORTABLE_DIRECTORY;
