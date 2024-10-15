c22cd0be0133be995f47296f1dc59c73cb9175f0
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2024-10-14 22:17:07 -0700

    Show tag hierarchy in Tag Selector (#1)
    * Show tag hierarchy in Tag Selector
    
    * Fixes for TS errors

--- src/frontend/components/TagSelector.tsx
+++ src/frontend/components/TagSelector.tsx
@@ -263,10 +263,12 @@ interface TagOptionProps {
 }
 
 export const TagOption = observer(({ id, tag, selected, toggleSelection }: TagOptionProps) => {
-  const [path, hint] = useComputed(() => {
-    const path = tag.path.join(' › ');
-    const hint = path.slice(0, Math.max(0, path.length - tag.name.length - 3));
-    return [path, hint];
+  const path = useComputed(() => {
+    return tag.path.join(' › ');
+  }).get();
+
+  const subTreeLevel = useComputed(() => {
+    return tag.path.length - 1;
   }).get();
 
   return (
@@ -277,8 +279,7 @@ export const TagOption = observer(({ id, tag, selected, toggleSelection }: TagOp
       icon={<span style={{ color: tag.viewColor }}>{IconSet.TAG}</span>}
       onClick={() => toggleSelection(selected ?? false, tag)}
       tooltip={path}
-    >
-      {hint.length > 0 ? <GridCell className="tag-option-hint">{hint}</GridCell> : <GridCell />}
-    </Row>
+      indent={subTreeLevel}
+    />
   );
 });
